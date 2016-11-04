1e03cd471cbc5163a89df51829e40cb2a9a1c535
jescriba <escribirajoshua@gmail.com>
2016-11-03 19:08:06 -0700

    Initial Commit

---
+++ Canvas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Canvas
+//
+//  Created by Joshua Escribano on 11/3/16.
+//  Copyright © 2016 JoshuaMike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
